d161cbbf0e38d7afe6eb8b2b7172353e62963396
Nguyễn Quang Huy <qhuy180624@gmail.com>
2025-08-27 15:18:45 +0700

    Setting up Postgres and ingest data into Postgres

---
+++ api_request.py
@@ -0,0 +1,59 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("api_key")
+print(api_key)
+
+api_url = f"http://api.weatherstack.com/current?access_key=575f890eb34d3452242406a560ed991d&query=New York"
+def fetch_data():
+    print("Fetching data from WeatherStack...")
+    try:    
+        response = requests.get(api_url)
+        response.raise_for_status()
+        print("API Response received successfully.")
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from API: {e}")
+        raise
+def fech_mock_data():
+    print("Fetching mock data...")
+    mock_data = {
+        "location": {
+            "name": "New York",
+            "country": "United States of America",
+            "region": "New York",
+            "lat": "40.714",
+            "lon": "-74.006",
+            "timezone_id": "America/New_York",
+            "localtime": "2023-10-01 10:00",
+            "localtime_epoch": 1696154400,
+            "utc_offset": "-4.0"
+        },
+        "current": {
+            "observation_time": "02:00 PM",
+            "temperature": 22,
+            "weather_code": 113,
+            "weather_icons": [
+                "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png"
+            ],
+            "weather_descriptions": [
+                "Sunny"
+            ],
+            "wind_speed": 13,
+            "wind_degree": 240,
+            "wind_dir": "WSW",
+            "pressure": 1012,
+            "precip": 0,
+            "humidity": 56,
+            "cloudcover": 0,
+            "feelslike": 24,
+            "uv_index": 5,
+            "visibility": 16,
+            "is_day": "yes"
+        }
+    }
+    return mock_data
+        
---
+++ insert_records.py
@@ -0,0 +1,3 @@
+from api_request import fetch_data, fech_mock_data
+
+print(fech_mock_data())
